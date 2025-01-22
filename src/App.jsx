11202cf8e9790ebefa8435cb9f0a93a9bf3e1390
Yusufia <Yusufiabichi@yahoo.com>
2025-01-22 06:44:09 -0800

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <h1 className="text-4xl font-bold text-blue-600 test">Hello, Tailwind CSS!</h1>
     </div>
     </>
   )
