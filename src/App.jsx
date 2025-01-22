e7724f9aa50cdc3cd3b8dcbe6189d04ce6eba136
Yusufia <Yusufiabichi@yahoo.com>
2025-01-22 06:19:50 -0800

    postcss

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <div>School Portal</div>
-      
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
     </>
   )
 }
