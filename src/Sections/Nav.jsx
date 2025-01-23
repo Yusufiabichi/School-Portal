38ad4c153f943d61e38b58311a81f42f3fa9e0ef
Yusufia <Yusufiabichi@yahoo.com>
2025-01-23 13:20:09 -0800

    nav links

--- src/Sections/Nav.jsx
+++ src/Sections/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="bg-blue-600 text-white">
+    <nav className="bg-blue-600 text-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         {/* Logo */}
         <div className="text-xl font-bold">
@@ -24,21 +24,23 @@ const Nav = () => {
         </button>
 
         {/* Nav Links */}
-        <ul
-          className={`absolute lg:static top-14 left-0 lg:flex lg:space-x-6 w-full lg:w-auto bg-blue-600 lg:bg-transparent transition-all duration-300 ${
-            isMenuOpen ? 'block' : 'hidden'
+        <div
+          className={`absolute lg:static top-14 left-0 z-10 lg:flex lg:space-x-6 w-full lg:w-auto bg-blue-600 lg:bg-transparent transition-all duration-300 ease-in-out overflow-hidden ${
+            isMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
           }`}
         >
-          <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
-            <a href="#home" className="block">Home</a>
-          </li>
-          <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
-            <a href="#about" className="block">About Us</a>
-          </li>
-          <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
-            <a href="#services" className="block">Services</a>
-          </li>
-        </ul>
+          <ul className="flex flex-col lg:flex-row">
+            <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
+              <a href="#home" className="block">Home</a>
+            </li>
+            <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
+              <a href="#about" className="block">About Us</a>
+            </li>
+            <li className="py-2 px-4 lg:py-0 hover:bg-blue-700 lg:hover:bg-transparent">
+              <a href="#services" className="block">Services</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
