f38c9d7a7b54b10783aeec9eb07066d367ed5712
Yusufia <Yusufiabichi@yahoo.com>
2025-01-24 22:54:09 -0800

    const

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,15 +1,25 @@
+import { label } from "framer-motion/client";
+
 export const Register = [
-    { label: "Surname", placeholder: "" },
+    { label: "Surname", placeholder: "surname" },
     { label: "First Name", placeholder: "First Name" },
     { label: "Middle Name", placeholder: "Middle Name" },
     { label: "Address", placeholder: "Residential Address" },
     { label: "State of Origin", placeholder: "State of Origin" },
     { label: "State of Resident", placeholder: "State of Resident" },
+    { label: "Local Government of Resident", placeholder: "LGA of Resident" },
+    { label: "Email Address", placeholder: "Enter your email" },
+    { label: "Phone No.", placeholder: "Phone No." },
+    { label: "Guardian's Phone Number", placeholder: "Guardian's Phone Number" },
+    { label: "Guardian's Name", placeholder: "Guardian's Name" },
+    { label: "Guardian's Address", placeholder: "Guardian's Address" }
+
 ];
 
-export const Login [
-    { label: "" }
-]
+export const Login = [
+    { label: "Username", placeholder: "Enter Your Username" },
+    { label: "Password", placeholder: "Enter Your Password" },
+];
 
 export const shoes = [
     {
